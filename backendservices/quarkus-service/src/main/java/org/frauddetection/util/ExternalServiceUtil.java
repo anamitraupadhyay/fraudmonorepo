5d2c80aea140cfd20b5885015c90f1498d218bc9
Anamitra Upadhyay <anamitraupadhyay@gmail.com>
2025-04-08 20:25:22 +0530

    Implement Ktor data anonymization microservice call in ExternalServiceUtil and except setting up the url part :chore_for_later

--- backendservices/quarkus-service/src/main/java/org/frauddetection/util/ExternalServiceUtil.java
+++ backendservices/quarkus-service/src/main/java/org/frauddetection/util/ExternalServiceUtil.java
@@ -38,4 +38,20 @@ public class ExternalServiceUtil {
         conn.disconnect();
         return new JSONObject(responseBuilder.toString());
     }
+    public static void callKtorDataAnonymizationMicroservice(JSONObject data) throws IOException {
+     System.out.println("Calling Ktor data anonymization microservce with no payload:" +data.toString());
+     URI uriobj = URI.create("http://.../anonymize"); //no need to use .toURL() here as newBuilder works with only URI
+     HttpRequest request = HttpRequest.newBuilder()
+             .uri(uriobj)
+             .header("Content-Type", "application/json")
+             //.setDoOutput(false) nope wont work, its of HttpURLConnection not Http
+             .POST(HttpRequest.BodyPublishers.ofString(data.toString()))
+             .build();
+             HttpClient client = HttpClient.newHttpClient();
+             client.senAsync(request, HttpResponse.Bodyhandler.discarding());
+             //this is a fire and forget request, no need to wait for response
+             //if you want to wait for response, use send() method instead of sendAsync()
+             //and handle the response accordingly
+             //also return type is void of the method no need to return anything, also If you're doing payload retrieval, then ofString() or ofByteArray() is required — and it’s already concise.
+    }
 }
