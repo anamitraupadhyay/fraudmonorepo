6071c0a0b178f3b8e4175483dde06eb466ad6e98
subhodeep mishra <aec.cse.subhodeepmishra@gmail.com>
2025-07-11 20:55:24 +0530

    initialized the spring ollama service into backend services directly

---
+++ backendservices/spring-ollama-service/src/main/java/com/springollama/spring_ollama_service/SpringOllamaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springollama.spring_ollama_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOllamaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOllamaServiceApplication.class, args);
+	}
+
+}
---
+++ backendservices/spring-ollama-service/src/test/java/com/springollama/spring_ollama_service/SpringOllamaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springollama.spring_ollama_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOllamaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
